f0a4ef91e9e167383a748293c3dd33bf2cce7156
kushal <kushalsharma57169@gmail.com>
2025-07-10 12:54:20 +0530

    Auto-run migrate on startup

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+
+# At the bottom of core/wsgi.py
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception:
+    pass
